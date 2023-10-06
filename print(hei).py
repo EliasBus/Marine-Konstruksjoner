8b685bd5b70e1f496a0652e321f01b96efcc01c6
EliasBus <147044038+EliasBus@users.noreply.github.com>
2023-10-06 16:19:18 +0200

    Update print(hei).py

--- print(hei).py
+++ print(hei).py
@@ -4,4 +4,5 @@ print("OKei gutta:")
 print("CTRL + SHIFT + P og så Git: sync for å oppdatere desktop vsCode")
 
 print("Fn + F5 for å oppdatere selve github-siden")
-          
+#Man kan også endre kode direkte i GIThub
+#Når noen andre endrer kode må man starte med å synce så man har den mest oppdaterte koden
