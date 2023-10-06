3917decac450b57e090992f5a955b41795681cce
Elias <eliasbus@stud.ntnu.no>
2023-10-06 16:21:18 +0200

    Ses på søndag

--- print(hei).py
+++ print(hei).py
@@ -6,3 +6,6 @@ print("CTRL + SHIFT + P og så Git: sync for å oppdatere desktop vsCode")
 print("Fn + F5 for å oppdatere selve github-siden")
 #Man kan også endre kode direkte i GIThub
 #Når noen andre endrer kode må man starte med å synce så man har den mest oppdaterte koden
+
+
+#Send melding hvis noe er uklart, ses på søndag tommel opp
